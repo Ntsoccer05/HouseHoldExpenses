1bfdb9128d768c57d905dad665f1a3418440c85b
Ntsoccer05 <2460socceronly0512@gmail.com>
2026-08-02 00:59:40 +0900

    infra/terraformのコード修正

--- src/app/Http/Middleware/DebugSessionTrace.php
+++ src/app/Http/Middleware/DebugSessionTrace.php
@@ -18,6 +18,8 @@ class DebugSessionTrace
     {
         $cookieName = config('session.cookie');
         $rawCookie = $request->cookie($cookieName);
+        $allCookieNames = array_keys($request->cookies->all());
+        $rawHeaderCookie = $request->headers->get('Cookie');
 
         $response = $next($request);
 
@@ -25,6 +27,10 @@ class DebugSessionTrace
             'micro' => microtime(true),
             'path' => $request->path(),
             'method' => $request->method(),
+            'configured_cookie_name' => $cookieName,
+            'all_cookie_names' => $allCookieNames,
+            'raw_cookie_header_present' => $rawHeaderCookie !== null,
+            'raw_cookie_header_len' => $rawHeaderCookie ? strlen($rawHeaderCookie) : 0,
             'raw_cookie_present' => $rawCookie !== null,
             'raw_cookie_len' => $rawCookie ? strlen($rawCookie) : 0,
             'raw_cookie_hash' => $rawCookie ? substr(md5($rawCookie), 0, 12) : null,
